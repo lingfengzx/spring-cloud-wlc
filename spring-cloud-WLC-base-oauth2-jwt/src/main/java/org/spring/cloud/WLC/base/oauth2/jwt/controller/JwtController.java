e4a90f428d7fab713af5d57dd8826cf36d210890
lingfengzx <15966480251@126.com>
2020-06-15 17:25:32 +0800

    解析JWTToken

--- spring-cloud-WLC-base-oauth2-jwt/src/main/java/org/spring/cloud/WLC/base/oauth2/jwt/controller/JwtController.java
+++ spring-cloud-WLC-base-oauth2-jwt/src/main/java/org/spring/cloud/WLC/base/oauth2/jwt/controller/JwtController.java
@@ -2,13 +2,15 @@ package org.spring.cloud.WLC.base.oauth2.jwt.controller;
 
 import org.spring.cloud.WLC.base.oauth2.jwt.util.JwtUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.jsonwebtoken.Claims;
 
-@RestController("/jwt")
+@RestController
+@RequestMapping("/jwt")
 public class JwtController {
+	
+	@RequestMapping("/parse")
 	public Claims jwtParse(String jsonWebToken) {
 		return JwtUtil.parseJWT(jsonWebToken);
 	}
